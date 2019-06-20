9e97e5d90b9de863d034ff1ffff3c4fca8a6f0b4
Quentin Neyraud <quentin.neyraud@gmail.com>
2019-06-20 14:57:59 +0200

    initial commit

---
+++ src/components/QImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="image">
+    <img :src="src" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
