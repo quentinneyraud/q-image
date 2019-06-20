99dd3b6312a654c5f604f0a02b23206704ce5afa
Quentin Neyraud <quentin.neyraud@gmail.com>
2019-06-20 16:52:36 +0200

    add all responsive config

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <q-image></q-image>
+    <q-image
+      class="app-image"
+      v-bind="image"/>
   </div>
 </template>
 
@@ -9,11 +11,47 @@ import QImage from './components/QImage.vue'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      image: {
+        alt: 'Une image d\'illustration',
+        src: require('./images/landscape.jpg'),
+        color: 'red',
+        placement: 'cover',
+        jpg: [{
+          source: require('./images/landscape_650.jpg'),
+          width: 650
+        }, {
+          source: require('./images/landscape_900.jpg'),
+          width: 900
+        }, {
+          source: require('./images/landscape_1400.jpg'),
+          width: 1400
+        }],
+        sizes: '(min-width: 1200px) 75vw, 100vw'
+      }
+    }
+  },
   components: {
     QImage
   }
 }
 </script>
 
-<style>
+<style lang="stylus">
+  html, body
+    margin 0
+    padding 0
+</style>
+
+<style lang="stylus" scoped>
+.app-image
+  width: 100vw;
+  height: 500px;
+
+  @media screen and (min-width: 1200px) {
+    display block
+    margin 0 auto
+    width 75vw
+  }
 </style>
--- src/components/QImage.vue
+++ src/components/QImage.vue
@@ -1,16 +1,176 @@
 <template>
-  <div class="image">
-    <img :src="src" alt="">
+  <div
+    class="q-image"
+    :class="{
+      loading: lazyload,
+      loaded: !lazyload
+    }"
+    :style="{ backgroundColor: color }">
+
+    <picture
+      class="image"
+      ref="image">
+      <source
+        v-if="jpg"
+        type="image/jpg"
+        :data-srcset="lazyload && jpgSrcSet"
+        :srcset="!lazyload && jpgSrcSet"
+        :sizes="sizes">
+      <source
+        v-if="webp"
+        type="image/webp"
+        :data-srcset="lazyload && webpSrcSet"
+        :srcset="!lazyload && webpSrcSet"
+        :sizes="sizes">
+      <img
+        ref="baseimage"
+        :data-src="lazyload && src"
+        :src="!lazyload && src"
+        :alt="alt"
+        :style="{
+          objectFit: placement,
+          objectPosition: position
+        }">
+    </picture>
   </div>
 </template>
 
 <script>
+// utils
+import { isMinLength } from '../validators.js'
+
 export default {
   props: {
+    // basic props
+    alt: {
+      type: String,
+      required: true,
+      default: 'Image non disponible',
+      validator: value => isMinLength(value, 5)
+    },
     src: {
       type: String,
       required: true
+    },
+    // repsonsive images
+    jpg: {
+      type: Array,
+      required: false,
+      default: null
+    },
+    webp: {
+      type: Array,
+      required: false,
+      default: null
+    },
+    sizes: {
+      type: String,
+      required: false,
+      default: null
+    },
+    // style props
+    placement: {
+      type: String,
+      required: false,
+      default: () => 'cover',
+      validator: (value) => ['contain', 'cover', 'none'].indexOf(value) > -1
+    },
+    position: {
+      type: String,
+      required: false,
+      default: () => 'center'
+    },
+    color: {
+      type: String,
+      required: false,
+      default: 'none'
+    },
+    // lazyload props
+    lazyload: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    lazyloadType: {
+      type: String,
+      required: false,
+      default: 'direct',
+      validator: (value) => ['direct', 'intersection', 'called'].indexOf(value) > -1
+    }
+  },
+  data () {
+    return {
+      loaded: false
+    }
+  },
+  computed: {
+    jpgSrcSet () {
+      if (!this.jpg) return ''
+      return this.jpg
+        .map(jpgSource => `${jpgSource.source} ${jpgSource.width}w`)
+        .join(', ')
+    },
+    webpSrcSet () {
+      if (!this.webp) return ''
+      return this.webp
+        .map(webpSource => `${webpSource.source} ${webpSource.width}w`)
+        .join(', ')
+    }
+  },
+  mounted () {
+    if (this.lazyload && this.lazyloadType === 'direct') {
+      this.lazyloadImage()
+    }
+
+    if (this.$refs.baseimage.src && this.$refs.baseimage.complete) {
+      this.onLoaded()
+    } else {
+      this.$refs.image.addEventListener('load', this.onLoaded)
+    }
+  },
+  methods: {
+    lazyloadImage () {
+      Array.from(this.$refs.image.querySelectorAll('source'))
+        .forEach(source => {
+          source.srcset = source.dataset.srcset
+        })
+      this.$refs.baseimage.src = this.$refs.baseimage.dataset.src
+    },
+    onIntersect () {
+      this.lazyloadImage()
+    },
+    onLoaded () {
+      this.$emit('loaded', this)
+      this.loaded = true
+      this.$el.classList.remove('loading')
     }
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+  .q-image
+    position relative
+    overflow hidden
+    transition opacity 0.3s
+    width 100%
+    height 100%
+
+    &.loading
+      opacity 0
+
+    &.loaded
+      opacity 1
+
+  .image
+    display inline-block
+    width 100%
+    height 100%
+
+
+  .image img
+    width 100%
+    height 100%
+    object-fit cover
+    user-select none
+</style>
